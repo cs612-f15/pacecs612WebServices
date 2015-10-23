9ba932aa7b52ec63278476f092913e54ee96dbda
Shawn Cicoria <github@cicoria.com>
2015-10-23 14:43:15 -0400

    initial

---
+++ src/SimpleServices/ConsoleApplication1/Program.cs
@@ -0,0 +1,33 @@
+﻿using ConsoleApplication1.PeopleService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("press enter to go...");
+
+            Console.ReadLine();
+
+            var client = new PeopleClient("HostedPeople");
+
+            //var client = new PeopleClient("local");
+
+            var rv = client.GetDataAsync(1).Result;
+
+            Console.WriteLine("{0} - {1} - {2}", rv.Id, rv.familyName, rv.givenName);
+
+            Console.WriteLine("press enter to go...");
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ src/SimpleServices/SimpleWcf/People.svc.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace SimpleWcf
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class People : IPeople
+    {
+        public Person GetData(int value)
+        {
+            //return new Person
+            //{
+            //    Id = 1,
+            //    familyName = "foo",
+            //    givenName = "bar"
+            //};
+
+
+            var ctx = new pacedpsEntities();
+
+            var person = ctx.People.Where(p => p.Id == value).FirstOrDefault();
+
+            return person;
+
+
+
+        }
+
+
+    }
+}
